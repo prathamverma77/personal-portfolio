a8e720d11fb43b194b0da5193d957badf40dc98c
Pratham <prathamverma1980@gmail.com>
2026-08-25 13:04:18 +0530

    just to redeploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import AboutMe from "@/components/sections/AboutMe";
 import MyStack from "@/components/sections/MyStack";
 import ExpTimeline from "@/components/sections/ExpTimeline";
 import Projects from "@/components/sections/Projects";
-import GithubStats from "@/components/sections/GithubStats";
+// import GithubStats from "@/components/sections/GithubStats";
 import Contact from "@/components/sections/Contact";
 
 export default function Home() {
